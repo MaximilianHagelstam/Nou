a43ddf9d800cdc7914fdc2dee04af34347c862b3
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-09-11 12:48:51 +0300

    Add bot boilerplate

---
+++ NouBot/Commands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace Noubot
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("test")]
+        public async Task Test()
+        {
+            await ReplyAsync("Hello!");
+        }
+    }
+}
